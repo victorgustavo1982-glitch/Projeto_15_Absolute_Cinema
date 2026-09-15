e7fee5972e692635aa154679b6c8fa88b49e94f6
Victor Gustavo <victor.gustavo1982@gmail.com>
2026-09-15 10:39:27 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -9,19 +9,28 @@ st.set_page_config(
 )
 
 # Carregar dados dos filmes
-
+filmes = pd.read_csv('filmes.csv')
 
 # Sidebar apenas com filtro de gênero
 # coloque a logo e a selectbox
-
+st.sidebar.title('NeyFlix')
+st.sidebar.image('logoneyma.png')
+st.sidebar.title("filtros")
+genero_escolhido = st.sidebar.selectbox(
+    "Filtar por Gênero",
+    ["Todos"] + sorted(filmes['Genre'].unique().tolist())
+)
 
 # Aplicar filtro
-
+if genero_escolhido == 'Todos':
+    filmes_filtrados = filmes
+else:
+    filmes_filtrados = filmes[filmes['Genre'] == genero_escolhido]
 
 # Título da página
-
+st.title('Top Filmes IMDB')
 # Mostrar quantidade de filmes
-
+st.write(f'**{len(filmes_filtrados)}Filmes Filtrados**')
 
 # Mostrar filmes em grid (3 colunas)
 colunas = st.columns(3)
@@ -37,8 +46,9 @@ for index, filme in filmes_filtrados.iterrows():
             st.image(filme['Image URL'], use_container_width=True)
             
             # Informações do filme
-
-            
+            st.write(f"**{filme['Title']}**")
+            st.write(f"Gênero: {filme['Genre']}")
+            st.write(f"Ano: {filme['Year']}")
             # Botão para ver no IMDb
             if st.button(f"Ver no IMDb", key=f"btn_{filme['Rank']}"):
                 st.markdown(f"[🔗 Abrir página do IMDb]({filme['IMDb URL']})")
